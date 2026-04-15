94122c160c40cc71bf30497e041397cf54e528a5
oscar2697 <lindooscar635@gmail.com>
2026-04-14 19:25:26 -0500

    Implement suggested fixes

--- src/routes/workouts.ts
+++ src/routes/workouts.ts
@@ -1,7 +1,7 @@
 import { and, desc, eq, getTableColumns, ilike, or, sql } from 'drizzle-orm'
 import express from 'express'
-import { departments, workouts } from '../db/schema'
-import { db } from '../db'
+import { departments, workouts } from '../db/schema/index.js'
+import { db } from '../db/index.js'
 
 const router = express.Router()
 
